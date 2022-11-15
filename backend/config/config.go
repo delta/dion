e3ae70f6fe684239c785835a702ac96df64d4832
Dipesh Kafle <dipesh.kaphle111@gmail.com>
2022-11-15 18:46:48 +0530

    Add module level docs

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,6 +1,17 @@
+//		config.yaml variable meanings
+//
+//	 - ${ENV} => This means we'll check environment variable "ENV" and set the value to whatever it is.
+//	   If it is empty or unset, it'll be set to that
+//
+//	 - ${ENV:dev} => This means if environment variable "ENV" is not set, we'll use the default
+//	   value i.e "dev"
+//
+//	 - ${ENV:-dev} => This means if environment variable "ENV" is not set or is set to
+//	   empty string,we'll use the default value "dev"
+//
+//	   For further information ,see the tests or understand the source code
+//	  or go through the config_test.go to understand how config works
 package config
-
-// Please go through the config_test.go to understand how config works
 
 import (
 	"fmt"
